4c792d32fcf57068f653a429aef119a25c65d500
Jrew <jrewmohamed@gmail.com>
2018-11-26 20:06:36 -0500

    added burgers_controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -13,7 +13,7 @@ router.get("/", function (req, res) {
 });
 
 router.post("/api/burgers", function (req, res) {
-    burger.createOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+    burger.createOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function (result) {
         res.json({ id: result.insertId });
     })
 });
@@ -22,5 +22,17 @@ router.put("/api/burgers/:id", function (req, res) {
     let condition = `id = ${req.params.id}`;
     console.log("Condition", condition);
 
-    burger.updateOne()
+    burger.updateOne({
+        devoured: req.body.devoured
+    }),
+    condition,
+    function (result) {
+        if (result.changedRows === 0) {
+            return res.status(404).end();
+        }
+        res.status(200).end();
+
+    }
 })
+
+module.exports = router;
