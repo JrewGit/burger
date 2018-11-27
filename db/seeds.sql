13241cb0ee88c145b55caaae40900b99428f5a06
Jrew <jrewmohamed@gmail.com>
2018-11-26 19:17:14 -0500

    added connection and orm

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES('Cheesy Cheese Burger', FALSE);
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES('Beautiful Bacon Burger', FALSE);
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES('Savory Sausage Burger', FALSE);
---
+++ server.js
@@ -0,0 +1,2 @@
+const express = require('express');
+// const expresshbrs = require('express-handlebars');
