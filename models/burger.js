4dcbb0b9c4bab5430360d61b95221ff3b52213c4
Jrew <jrewmohamed@gmail.com>
2018-11-26 19:32:38 -0500

    added models and burger.js

---
+++ models/burger.js
@@ -0,0 +1,21 @@
+const orm = require('../config/orm');
+
+let burger = {
+    selectAll: function (cb) {
+        orm.selectAll('burgers', function (res) {
+            cb(res);
+        });
+    },
+    createOne: function (cols, vals, cb) {
+        orm.createOne('burgers', cols, vals, function (res) {
+            cb(res);
+        });
+    },
+    updateOne: function (objColVals, condition, cb) {
+        orm.updateOne('burgers', cols, vals, function (res) {
+            cb(res);
+        })
+    }
+}
+
+module.exports = burger;
