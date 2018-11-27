2b40c830c5d7b131585d113b255a01733ca041a3
Jrew <jrewmohamed@gmail.com>
2018-11-27 16:30:31 -0500

    working on handlebars

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
 const express = require('express');
-// const expresshbrs = require('express-handlebars');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers/burgers_controller')
+
+let PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(routes);
+
+app.listen(PORT, function () {
+    console.log("App now listening at localhost:" + PORT);
+});
